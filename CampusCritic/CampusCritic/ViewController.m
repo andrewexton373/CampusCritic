0285562ef44b8a13f6ee5b0b3b22d843a28ba00a
andrewexton373 <andrewexton@Andrews-MacBook-Pro.local>
2013-10-25 13:39:48 -0700

    Created Xcode Project

---
+++ CampusCritic/CampusCritic/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CampusCritic
+//
+//  Created by Andrew Exton on 10/25/13.
+//  Copyright (c) 2013 CCDevelopment. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
