16c73489f537dbaca6dad8da3cfc8c7a92eb8db4
andrewexton373 <andrewexton@Andrews-MacBook-Pro.local>
2013-10-25 13:49:58 -0700

    Created Navigation Controller and Main Food List Table View

---
+++ CampusCritic/CampusCritic/FoodItemsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodItemsTableViewController.h
+//  CampusCritic
+//
+//  Created by Andrew Exton on 10/25/13.
+//  Copyright (c) 2013 CCDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodItemsTableViewController : UITableViewController
+
+@end
